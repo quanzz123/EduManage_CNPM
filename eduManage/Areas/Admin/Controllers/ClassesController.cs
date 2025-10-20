b4795a618f2e14f923d9665f06cc93463b85946f
quanzz123 <nguyenquandc37@gmail.com>
2025-10-20 17:31:11 +0700

    classes/create

--- eduManage/Areas/Admin/Controllers/ClassesController.cs
+++ eduManage/Areas/Admin/Controllers/ClassesController.cs
@@ -1,5 +1,6 @@
 ﻿using eduManage.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace eduManage.Areas.Admin.Controllers
@@ -17,6 +18,37 @@ namespace eduManage.Areas.Admin.Controllers
             var classList = _context.TblClasses.Include(m => m.Teacher).OrderBy(m => m.ClassId).ToList();
 
             return View(classList);
+        }
+
+        public IActionResult Create()
+        {
+            var teachers = (from t in _context.TblUsers
+                            .Where(u => u.RoleId == 2)
+                            select new SelectListItem()
+                            {
+                                Text = t.FullName,
+                                Value = t.UserId.ToString()
+                            }
+                            ).ToList();
+            teachers.Insert(0, new SelectListItem()
+            {
+                Text = "--Select Teacher--",
+                Value = "0"
+            });
+            ViewBag.TeacherList = teachers;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(TblClass? cls)
+        {
+            if (ModelState.IsValid)
+            {
+                cls.CreateDate = DateTime.Now;
+                _context.TblClasses.Add(cls);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cls);
         }
     }
 }
