63abc80a25263a6be15f3de8f793c2dea3c6ff71
quanzz123 <nguyenquandc37@gmail.com>
2025-10-21 10:00:55 +0700

    classmembers/create

---
+++ eduManage/Areas/Admin/Controllers/ClassMembersController.cs
@@ -0,0 +1,37 @@
+﻿using eduManage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eduManage.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ClassMembersController : Controller
+    {
+        private readonly EdumanageContext _context;
+        public ClassMembersController(EdumanageContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int id)
+        {
+            var memberList = _context.TblClassMembers
+                .Join(_context.TblUsers,
+                      cm => cm.UserId,
+                      u => u.UserId,
+                      (cm, u) => new TblClassMember
+                      {
+                          MemberId = cm.MemberId,
+                          ClassId = cm.ClassId,
+                          JoinDate = cm.JoinDate,
+                          Status = cm.Status,
+                          Progress = cm.Progress,
+                          FinalScore = cm.FinalScore,
+                          Note = cm.Note,
+                          UserId = cm.UserId,
+                          User = u
+                      })
+                .Where(m => m.ClassId == id)
+                .ToList();
+            return View(memberList);
+        }
+    }
+}
--- eduManage/Areas/Admin/Controllers/ClassesController.cs
+++ eduManage/Areas/Admin/Controllers/ClassesController.cs
@@ -4,51 +4,51 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace eduManage.Areas.Admin.Controllers
-{
-    [Area("Admin")]     
-    public class ClassesController : Controller
     {
-        private readonly EdumanageContext _context;
-        public ClassesController(EdumanageContext context)
+        [Area("Admin")]     
+        public class ClassesController : Controller
         {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            var classList = _context.TblClasses.Include(m => m.Teacher).OrderBy(m => m.ClassId).ToList();
-
-            return View(classList);
-        }
-
-        public IActionResult Create()
-        {
-            var teachers = (from t in _context.TblUsers
-                            .Where(u => u.RoleId == 2)
-                            select new SelectListItem()
-                            {
-                                Text = t.FullName,
-                                Value = t.UserId.ToString()
-                            }
-                            ).ToList();
-            teachers.Insert(0, new SelectListItem()
+            private readonly EdumanageContext _context;
+            public ClassesController(EdumanageContext context)
             {
-                Text = "--Select Teacher--",
-                Value = "0"
-            });
-            ViewBag.TeacherList = teachers;
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Create(TblClass? cls)
-        {
-            if (ModelState.IsValid)
-            {
-                cls.CreateDate = DateTime.Now;
-                _context.TblClasses.Add(cls);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                _context = context;
             }
-            return View(cls);
+            public IActionResult Index()
+            {
+                var classList = _context.TblClasses.Include(m => m.Teacher).OrderBy(m => m.ClassId).ToList();
+
+                return View(classList);
+            }
+
+            public IActionResult Create()
+            {
+                var teachers = (from t in _context.TblUsers
+                                .Where(u => u.RoleId == 2)
+                                select new SelectListItem()
+                                {
+                                    Text = t.FullName,
+                                    Value = t.UserId.ToString()
+                                }
+                                ).ToList();
+                teachers.Insert(0, new SelectListItem()
+                {
+                    Text = "--Select Teacher--",
+                    Value = "0"
+                });
+                ViewBag.TeacherList = teachers;
+                return View();
+            }
+            [HttpPost]
+            public async Task<IActionResult> Create(TblClass? cls)
+            {
+                if (ModelState.IsValid)
+                {
+                    cls.CreateDate = DateTime.Now;
+                    _context.TblClasses.Add(cls);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(cls);
+            }
         }
     }
-}
