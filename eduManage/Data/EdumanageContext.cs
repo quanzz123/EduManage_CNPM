781769da3203822ff869e842886135ff033dadcb
quin21-05 <117883907+quin21-05@users.noreply.github.com>
2025-10-21 18:11:23 +0700

    Edit class and classmember

---
+++ eduManage/Data/EdumanageContext.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using eduManage.Models;
+
+namespace eduManage.Data;
+
+public partial class EdumanageContext : DbContext
+{
+    public EdumanageContext()
+    {
+    }
+
+    public EdumanageContext(DbContextOptions<EdumanageContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblClass> TblClasses { get; set; }
+
+    public virtual DbSet<TblClassMember> TblClassMembers { get; set; }
+
+    public virtual DbSet<TblRole> TblRoles { get; set; }
+
+    public virtual DbSet<TblUser> TblUsers { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblClass>(entity =>
+        {
+            entity.HasKey(e => e.ClassId);
+
+            entity.ToTable("tblClasses");
+
+            entity.Property(e => e.ClassId).HasColumnName("ClassID");
+            entity.Property(e => e.ClassName).HasMaxLength(250);
+            entity.Property(e => e.CreateDate).HasColumnType("datetime");
+            entity.Property(e => e.EndDate).HasColumnType("datetime");
+            entity.Property(e => e.Image).HasMaxLength(250);
+            entity.Property(e => e.ModifedDate).HasColumnType("datetime");
+            entity.Property(e => e.Schedule).HasMaxLength(50);
+            entity.Property(e => e.StartDate).HasColumnType("datetime");
+            entity.Property(e => e.Subject).HasMaxLength(250);
+            entity.Property(e => e.TeacherId).HasColumnName("TeacherID");
+
+            entity.HasOne(d => d.Teacher).WithMany(p => p.TblClasses)
+                .HasForeignKey(d => d.TeacherId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_tblClasses_tblUsers");
+        });
+
+        modelBuilder.Entity<TblClassMember>(entity =>
+        {
+            entity.HasKey(e => e.MemberId);
+
+            entity.ToTable("tblClassMembers");
+
+            entity.Property(e => e.MemberId).HasColumnName("MemberID");
+            entity.Property(e => e.ClassId).HasColumnName("ClassID");
+            entity.Property(e => e.JoinDate).HasColumnType("datetime");
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Class).WithMany(p => p.TblClassMembers)
+                .HasForeignKey(d => d.ClassId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_tblClassMembers_tblClasses");
+
+            entity.HasOne(d => d.User).WithMany(p => p.TblClassMembers)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_tblClassMembers_tblUsers");
+        });
+
+        modelBuilder.Entity<TblRole>(entity =>
+        {
+            entity.HasKey(e => e.RoleId);
+
+            entity.ToTable("tblRoles");
+
+            entity.Property(e => e.RoleDescription).HasMaxLength(250);
+            entity.Property(e => e.RoleName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<TblUser>(entity =>
+        {
+            entity.HasKey(e => e.UserId);
+
+            entity.ToTable("tblUsers");
+
+            entity.Property(e => e.Address).HasMaxLength(150);
+            entity.Property(e => e.CreateDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.PassworkHash).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.UserName).HasMaxLength(50);
+
+            entity.HasOne(d => d.Role).WithMany(p => p.TblUsers)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK_tblUsers_tblRoles");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
