4d3a9a4c2729dca8cc36c8ef4116bc8666e347ff
quanzz123 <nguyenquandc37@gmail.com>
2025-12-16 21:35:25 +0700

    hask pass

--- eduManage/Utilities/Functions.cs
+++ eduManage/Utilities/Functions.cs
@@ -11,28 +11,40 @@ namespace eduManage.Utilities
         public static string _Email = String.Empty;
         public static string _Message = string.Empty;
         public static string _MessageEmail = string.Empty;
-        public static string MD5Hash(string text)
-        {
-            MD5 md5 = new MD5CryptoServiceProvider();
-            md5.ComputeHash(System.Text.Encoding.ASCII.GetBytes(text));
-            byte[] result = md5.Hash;
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < result.Length; i++)
-            {
-                strBuilder.Append(result[i].ToString("x2"));
 
-            }
-            return strBuilder.ToString();
+        public static string HashPassword(string password)
+        {
+            // Tạo salt ngẫu nhiên
+            byte[] salt = RandomNumberGenerator.GetBytes(16);
+
+            // Dùng PBKDF2 để hash password
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            // Ghép salt + hash -> lưu hoặc trả về dạng base64
+            byte[] hashBytes = new byte[salt.Length + hash.Length];
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, salt.Length);
+            Buffer.BlockCopy(hash, 0, hashBytes, salt.Length, hash.Length);
+
+            return Convert.ToBase64String(hashBytes);
         }
 
-        public static string MD5Password(string? text)
+        public static bool VerifyPassword(string password, string storedHash)
         {
-            string str = MD5Hash(text);
-            for (int i = 0; i < 1000; i++)
-                {
-                    str = MD5Hash(str+ "_" + str);
-                }
-            return str;
+            byte[] hashBytes = Convert.FromBase64String(storedHash);
+            byte[] salt = new byte[16];
+            Buffer.BlockCopy(hashBytes, 0, salt, 0, salt.Length);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            for (int i = 0; i < 32; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+            }
+
+            return true;
         }
     }
 }
